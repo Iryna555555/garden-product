3035b49b8c904c87e0b6f97f3ab7689b564b25d9
Iryna Pokazachenko <irynapokazachenko@gmail.com>
2025-04-18 09:47:10 +0200

    delate copy

--- src/pages/Categories/Categories.jsx
+++ src/pages/Categories/Categories.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import "./categories.scss";
+import "./Categories.scss";
 import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import Breadcrumbs from '../../components/Breadcrumbs/Breadcrumbs';
